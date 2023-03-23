6754aa8eff637b11a1131beb132f52c6e0ce4c4b
gabrielferraz97 <gabrielcma_@hotmail.com>
2023-03-23 23:38:58 +0000

    Script IaC Servidor Apache

---
+++ script_apache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Atualizando servidor"
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando os programs"
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main/
+cp -R * /var/www/html/
